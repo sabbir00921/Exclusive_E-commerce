f283c363dfb9fc2a1611dc07177f6149b0fc86a5
sabbir00921 <sabbir00921@gmail.com>
2025-09-24 19:46:01 +0600

    BestSelling file issue solve

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import Header from "../../components/rootLayout/Header";
-import Navbar from "../../components/rootLayout/Navbar/Index";
-import Banner from "../../components/homePage/Banner/Index";
-import FlashSale from "../../components/homePage/FlashSale/Index";
-import Category from "../../components/homePage/Category/Index";
-import Bestselling from "../../components/homePage/BestSelling/Index";
-import Experience from "../../components/homePage/Experience/index";
+import Header from "../../components/rootLayout/Header/Index";
+import Navbar from "./../../components/rootLayout/Navbar/Index";
+import Banner from "./../../components/homePage/Banner/Index";
+import FlashSale from "./../../components/homePage/FlashSale/Index";
+import CategoryIndex from "./../../components/homePage/Category/Index";
+import Bestselling from "./../../components/homePage/BestSelling/Index";
+import Experience from "./../../components/homePage/Experience/index";
 
 const Homepage = () => {
   return (
@@ -14,7 +14,7 @@ const Homepage = () => {
       <Navbar />
       <Banner />
       <FlashSale />
-      <Category />
+      <CategoryIndex />
       <Bestselling />
       <Experience />
     </div>
